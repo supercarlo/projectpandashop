82c0d91eda117a9aabc5ef86cc32187122164cc3
Carlo Brauwers <c.brauwers@hotmail.com>
2016-10-26 13:18:05 +0200

    tdxujkrs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Html html = new Html();
 //        DBC dbc = new DBC();
-//
+
 //        dbc.Connection();
         Spark.staticFileLocation("/");
 
